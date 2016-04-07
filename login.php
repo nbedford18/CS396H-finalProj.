09065c98a97a291ab1172db45a73b2c43f85e60b
Laura Delaney <lauracatspam2@gmail.com>
2016-04-06 21:13:41 -0700

    Update login.php -- bughunting
    even further back to the basics -- just an echo

--- login.php
+++ login.php
@@ -1,15 +1,11 @@
 <!DOCTYPE html>
 <html>
-<title>
-	database login page
-</title>
 <body>
 <?php>
  	//$username =  $_POST["name"];
  	//$password = $_POST["password"];
 	 //$arrivaltime;
- 
-	<h1> Welcome </h1>
+
 	//ini_set('display_errors', 1);
 	echo "your name is: ";
 	//echo $_POST["name"]; 

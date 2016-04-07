43b60a41eebf4d2ccc79cedcaeb4e34f364d7c52
Laura Delaney <lauracatspam2@gmail.com>
2016-04-06 20:48:13 -0700

    Create login.php
    doesn't connect with the server yet, just echoes the form

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<title>
+	database login page
+</title>
+<body>
+<?php>
+ $username;
+ $password;
+ $arrivaltime;
+
+	Welcome <?php echo $_POST["name"]; ?><br>
+	Your email address is: <?php echo $_POST["password"]; ?> 
+//don't display password
+
+
+<?>
+</body>
+</html>

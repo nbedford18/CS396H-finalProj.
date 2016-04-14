294fcd56c8b0443fd77e10f95e8fd6ca9c30bf87
Laura Delaney <lauracatspam2@gmail.com>
2016-04-13 20:34:01 -0700

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
  	$username =  $_POST["name"];
  	$password = $_POST["password"];
 	$arrivaltime = time();
-
+	$arrivaldate = getdate($arrivaltime);
 	//ini_set('display_errors', 1);
 	echo "your name is: ";
 	echo $username; 
@@ -13,18 +13,8 @@
 	echo "Your password is: ";
 	echo $password; 
 	echo "<br>";
-	//echo "The current time is $arrivaltime[hours] : $arrivaltime[minutes] : $arrivaltime[seconds], $arrivaltime[month], $arrivaltime[mday] , $arrivaltime[year]";
-	echo $arrivaltime[hours];
+	echo "The current time is $arrivaldate[hours] : $arrivaldate[minutes] : $arrivaldate[seconds], $arrivaldate[month], $arrivaldate[mday] , $arrivaldate[year]";
 	//print_r (getDate($arrivaltime));
-	
-	// Print the array from getdate()
-	print_r(getdate());
-	echo "<br><br>";
-
-	// Return date/time info of a timestamp; then format the output
-	$mydate=getdate(date("U"));
-	echo "$mydate[weekday], $mydate[month] $mydate[mday], $mydate[year]";
-	
 ?>
 </body>
 </html>

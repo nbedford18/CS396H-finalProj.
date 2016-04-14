ee418fc9915f41b2f473245bf184f23e41dff0ed
Laura Delaney <lauracatspam2@gmail.com>
2016-04-13 20:31:30 -0700

    Update login.php

--- login.php
+++ login.php
@@ -17,6 +17,14 @@
 	echo $arrivaltime[hours];
 	//print_r (getDate($arrivaltime));
 	
+	// Print the array from getdate()
+	print_r(getdate());
+	echo "<br><br>";
+
+	// Return date/time info of a timestamp; then format the output
+	$mydate=getdate(date("U"));
+	echo "$mydate[weekday], $mydate[month] $mydate[mday], $mydate[year]";
+	
 ?>
 </body>
 </html>
